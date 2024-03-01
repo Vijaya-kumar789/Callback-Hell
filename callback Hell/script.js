e15c32e86f6bdb6069722b2dcbe46c90067f3149
vijaya-kumar789 <vijaykumaryoyo8@gmail.com>
2024-03-01 13:37:02 +0530

    This is the callback hell task

---
+++ callback Hell/script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById("content").innerText="Countdown : 10 "
+    setTimeout(()=>{
+        document.getElementById("content").innerText="Countdown : 9"
+        setTimeout(()=>{
+            document.getElementById("content").innerText="Countdown : 8"
+            setTimeout(()=>{
+                document.getElementById("content").innerText="Countdown : 7"
+                setTimeout(()=>{
+                    document.getElementById("content").innerText="Countdown : 6"
+                    setTimeout(()=>{
+                        document.getElementById("content").innerText="Countdown : 5"
+                        setTimeout(()=>{
+                            document.getElementById("content").innerText="Countdown : 4"
+                            setTimeout(()=>{
+                                document.getElementById("content").innerText="Countdown : 3"
+                                setTimeout(()=>{
+                                    document.getElementById("content").innerText="Countdown : 2"
+                                    setTimeout(()=>{
+                                        document.getElementById("content").innerText="Countdown : 1"
+                                        setTimeout(()=>{
+                                            document.getElementById("content").innerText="Happy Independence Day!!!";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
